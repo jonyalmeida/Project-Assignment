f234b4ce0d17a12c58207232041411c413f64493
jon <jon@jons-MacBook-Pro.local>
2020-12-08 21:15:36 -0600

    success parse data

--- matchRespondentsFunction.js
+++ matchRespondentsFunction.js
@@ -15,15 +15,35 @@ async function readData(respondentsDataFilePath) {
 
     // Parse data into organized usable data
     // Split data into lines as 1 entry === 1 line
-    const parseLines = respondentsData.split("\n");
+    const parseLines = respondentsData.split("\n").slice(1);
 
     // Map over array of entry lines
     // Split lines into entry with key(categories) value(information) pairs
     // Formats values to lower case
-    parseLines.map((line) => {
-        const entry = line.toLowerCase().split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
-        console.log(entry);
+    // Stores organized data in respondentsDataObject using the line # index as keys
+    respondentsDataObject = {};
+
+    parseLines.map((line, idx) => {
+        if (line) {
+            const entry = line
+                .toLowerCase()
+                .split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+
+            respondentsDataObject[idx] = {
+                firstName: entry[0],
+                gender: entry[1],
+                jobTitle: entry[2],
+                industry: (() =>
+                    entry[3].slice(1, entry[3].length - 1).split(","))(),
+                city: (() =>
+                    entry[4].slice(1, entry[4].length - 1).split(",")[0])(),
+                lat: Number(entry[5]),
+                lon: Number(entry[6]),
+            };
+        }
     });
+
+    console.log(respondentsDataObject);
 }
 
 console.log(
